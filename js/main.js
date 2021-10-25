08e529869a8af67b23636586b42110aa365fdc59
klimov-rv <klimov@perco.ru>
2021-10-25 13:19:16 +0300

    giphy

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+MOCK_URL_SOURCE = [
+    "ehZqNokVylyWk",
+    "hDqq4LalRAUiQ",
+    "yjTccXlnh6LXW",
+    "3FjEPbKqEPhPpmC8uY",
+    "3ohs7NLUXtNW98mtIQ"
+];
+
+randomItem = () => {
+    return this.MOCK_URL_SOURCE[
+        Math.floor(Math.random() * this.MOCK_URL_SOURCE.length)
+    ];
+};
+
+
 const refactorProdMap = new Map([
     ['0_0', ['st01', '1']],
     ['0_1', ['st01', '2']],
@@ -25,6 +40,7 @@ let resolution_s = "320x182";
 let resolution_lg = "586x330";
 
 
+
 function startPlayback(video) {
     return video.play();
 }
@@ -33,8 +49,8 @@ async function asyncVideo(video) {
     const awaitedVid = await startPlayback(video).then(function() {
         console.log('The play() Promise fulfilled! Rock on!');
         video.oncanplaythrough = () => video.classList.remove("is-loading");
-        video.pause();
-        console.log("pause из playPromise-asyncVidLoad");
+        // video.pause();
+        // console.log("pause из playPromise-asyncVidLoad");
     }).catch(error => {
         console.log('The play() Promise rejected!');
         console.log('Use the Play button instead.');
@@ -56,11 +72,13 @@ function InsertCorrectVideo(mapVidOptions, resolution) {
                 console.log(video.canPlayType('video/mp4'));
                 console.log(video.canPlayType('video/webm'));
                 if (video.canPlayType('video/mp4').length > 0) {
+
                     var typeVid = 'video/mp4; codecs="avc1.4D401E, mp4a.40.2"';
-                    var pathToVid = `video/${model[0]}/${resolution}/${model[1]}.mp4`;
+                    var urlVid = `https://media0.giphy.com/media/${randomItem()}/giphy.mp4`;
+                    // var pathToVid = `video/${model[0]}/${resolution}/${model[1]}.mp4`;
                 } else if (video.canPlayType('video/webm').length > 0) {
-                    var typeVid = 'video/webm';
-                    var pathToVid = `video/${model[0]}/${resolution}/webm/${model[1]}.webm`;
+                    // var typeVid = 'video/webm';
+                    // var pathToVid = `video/${model[0]}/${resolution}/webm/${model[1]}.webm`;
                 } else {
                     var pathToVid = "";
                 }
@@ -71,7 +89,7 @@ function InsertCorrectVideo(mapVidOptions, resolution) {
                         sourceEl[index].parentNode.removeChild(sourceEl[index]);
                     };
                 }
-                vidSource.setAttribute('src', pathToVid);
+                vidSource.setAttribute('src', urlVid);
                 vidSource.setAttribute('type', typeVid);
 
                 video.appendChild(vidSource);
